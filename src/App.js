892ebca1b18a1235b276c3d7c76c5f8362264203
Marosh Sykora <sykoramaros@gmail.com>
2024-12-14 21:16:17 +0100

    pages - home, privacy, students, subjects, grades upraveny a propojeny s App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Privacy from "./pages/privacy/Privacy"
+import Students from "./pages/students/Students"
+import Subjects from "./pages/subjects/Subjects"
 
 const App = () => {
-  return <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<h1>Home</h1>}></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/subjects" element={<Subjects />} />
       </Routes>
-  </BrowserRouter>
+    </BrowserRouter>
+  )
 }
-
 export default App
---
+++ src/pages/privacy/privacy.js
@@ -0,0 +1,9 @@
+const Privacy = () => {
+  return (
+    <div>
+      <h1>Privacy</h1>
+    </div>
+  )
+}
+
+export default Privacy
