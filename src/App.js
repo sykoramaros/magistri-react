53e2c1145a17d63cbe172f345fbf4ea7be1caa89
Marosh Sykora <sykoramaros@gmail.com>
2024-12-14 07:29:31 +0100

    Zakladne upravena verze s React + Bootstrap pro zacatek programovani

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const student = {
+    name: "Petr",
+    lastname: "Novak",
+  }
 
-export default App;
+  const showAlert = () => {
+    alert("Hello " + student.name + " " + student.lastname)
+  }
+
+  return (
+    <div>
+      <h1 class="display-1">
+        {student.name + " " + student.lastname}
+      </h1>
+      <h2>Hello</h2>
+      <button class="btn btn-primary" onClick={showAlert}>Click</button>
+    </div>
+  )
+}
+export default App
