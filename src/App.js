34a017f4e3e1bbff83f0dd77da33401533eee5fb
Marosh Sykora <sykoramaros@gmail.com>
2024-12-14 17:44:42 +0100

    pages - home, privacy, students, subjects, grades pridany

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
-import React, { useEffect, useState } from "react"
-import axios from "axios"
-import { getStudents } from "./services/studentServices/getStudents"
-import { addStudent } from "./services/studentServices/addStudent"
-import { updateStudent } from "./services/studentServices/updateStudent"
-import { deleteStudent } from "./services/studentServices/deleteStudent"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [students, setStudents] = useState([])
-
-  useEffect(() => {
-    getStudents()
-      .then((response) => setStudents(response.data))
-      .catch((error) => console.error("Error listing students", error))
-  }, [])
-
-  const handleDeleteStudent = (id) => {
-    deleteStudent(id)
-      .then((response) =>
-        setStudents(students.filter((student) => student.id !== id))
-      )
-      .catch((error) => console.error("Error deleting student", error))
-  }
-
-  return (
-    <div>
-      <h1>Students list</h1>
-      <ul>
-        {students.map((student) => (
-          <li key={student.id}>
-            <p>
-              {student.firstName} {student.lastName}
-            </p>
-            <button
-              className="btn btn-danger"
-              onClick={() => handleDeleteStudent(student.id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+const App = () => {
+  return <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+  </BrowserRouter>
 }
+
 export default App
